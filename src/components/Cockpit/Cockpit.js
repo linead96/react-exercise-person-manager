4035fec679828783b3c53ed5d1ec451d3f36fff7
Linead96 <danielgarboza@yahoo.com>
2019-04-20 01:31:09 +0800

    implemented react.memo() to Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
-const cockpit = ( props ) => {
+const cockpit = props => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request
@@ -28,10 +28,10 @@ const cockpit = ( props ) => {
       btnClass = classes.Red;
   }
 
-  if ( props.persons.length <= 2 ) {
+  if ( props.personsLength <= 2 ) {
     assignedClasses.push( classes.red ); // classes = ['red']
   }
-  if ( props.persons.length <= 1 ) {
+  if ( props.personsLength <= 1 ) {
     assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
   }
 
@@ -46,4 +46,4 @@ const cockpit = ( props ) => {
   );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
