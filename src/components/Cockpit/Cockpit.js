7233d0fe1f8c93a248721a4f78be111336f2b95a
Linead96 <danielgarboza@yahoo.com>
2019-04-20 12:32:10 +0800

    added prop chain problem example

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -49,6 +49,9 @@ const cockpit = props => {
           >
             Toggle Persons
           </button>
+          <button onClick={props.login}>
+            Log in
+          </button>
       </div>
   );
 };
