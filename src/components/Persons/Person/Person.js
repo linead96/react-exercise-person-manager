24d617e041446fec7fb194112628bc1ab2aa8d27
Linead96 <danielgarboza@yahoo.com>
2019-04-20 13:30:04 +0800

    updated how authcontext were integrated in Cockpit.js and Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,21 +12,21 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log('[Person.js] this.context.authenticated',this.context.authenticated);
   }
 
   render() {
     console.log('[Person.js] rendering...');
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {
-            context => 
-              context.authenticated ? <p>Authenticated!</p>: <p>Please Login!</p>
-          }
-        </AuthContext.Consumer>
+        {
+          this.context.authenticated ? <p>Authenticated!</p>: <p>Please Login!</p>
+        }
         <p onClick={this.props.click}>
         I'm {this.props.name} and I am {this.props.age} years old!
         </p>
